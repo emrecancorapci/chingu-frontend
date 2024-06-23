bde7ece02f0aea54549f900445fe6f5dcc869fb0
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-06-24 00:23:43 +0300

    Init shadcn/ui, Add button and input components

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from './components/layout/header';
+
 export default function Layout() {
   return (
     <div className="flex h-screen w-full flex-col gap-2 bg-black p-2">
+      <Header />
       <Outlet />
     </div>
   );
